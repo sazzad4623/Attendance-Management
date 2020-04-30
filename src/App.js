e9b2753e1590c9a83114e8accf952d4d2bb6917f
sazzad4623 <sazzad111111@gmail.com>
2020-05-01 02:30:14 +0600

    navbar footer added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { Fragment } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Manual from "./pages/manualAttendance";
+import MiniDrawer from "./components/drawer.component";
+import ResponsiveDrawer from "./components/navbar.component";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Homepage from "./components/homepage.component";
+import StickyFooter from "./components/footer.component";
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Manual />
-      </div>
+      <Router>
+        <Fragment>
+          {" "}
+          <MiniDrawer />
+          {/* <ResponsiveDrawer /> */}
+          <center>
+            <Route
+              exact
+              path="/"
+              render={() => (
+                <div>
+                  <Homepage />
+                </div>
+              )}
+            ></Route>
+          </center>
+          <Route
+            path="/manualattendance"
+            render={() => (
+              <div>
+                <Manual />
+              </div>
+            )}
+          ></Route>
+          <StickyFooter />
+        </Fragment>
+      </Router>
     );
   }
 }
